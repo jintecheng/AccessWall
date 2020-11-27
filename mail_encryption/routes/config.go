2a8096b8d0666cdd8fbadcca4cac9d069dae7f4c
mcocma <mcocma@naver.com>
2020-11-27 09:21:13 +0900

    이메일 암호화 한글버전 config.go 내 함수 및 쿼리 변경

--- mail_encryption/routes/config.go
+++ mail_encryption/routes/config.go
@@ -4,7 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
@@ -18,16 +21,25 @@ type accountInfo struct {
 	mail_passwd string
 }
 
-const (
-	host     = "13.124.175.231"
-	port     = 5432
-	user     = "jhkim"
-	password = "qwer1234"
-	dbname   = "emailtest"
+var (
+	host     string
+	port     int
+	user     string
+	password string
+	dbname   string
+	chk      = false
 )
 
 // DB에 연결
 func connectDB() *sql.DB {
+	if chk != true {
+		chk = true
+		port, _ = strconv.Atoi(os.Getenv("DB_PORT"))
+		host = os.Getenv("DB_HOST")
+		user = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		dbname = os.Getenv("DB_NAME")
+	}
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
@@ -39,6 +51,7 @@ func connectDB() *sql.DB {
 		panic(err)
 	}
 
+	fmt.Println("DB_연결")
 	return db
 }
 
@@ -51,18 +64,18 @@ func disconnectDB(db *sql.DB) error {
 	return err
 }
 
-func getAccountInfo(db *sql.DB, mailadd string) accountInfo {
+func getAccountInfo(db *sql.DB, mailadd string, id string) accountInfo {
 
 	var accountinfo accountInfo
 
-	db.QueryRow("SELECT smtp_add FROM mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.smtp_add)
-	db.QueryRow("SELECT smtp_port FROM mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.smtp_port)
-	db.QueryRow("SELECT imap_add FROM mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.imap_add)
-	db.QueryRow("SELECT imap_port FROM mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.imap_port)
-	db.QueryRow("SELECT pop3_add FROM mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.pop3_add)
-	db.QueryRow("SELECT pop3_port FROM mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.pop3_port)
-	db.QueryRow("SELECT convert_from(decrypt(decode(mail_passwd, 'hex'), 'epjtwihnsasdc', 'aes'),'utf-8') from mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.mail_passwd)
-
+	db.QueryRow("SELECT smtp_add FROM mail_info WHERE mail=$1 and id=$2", mailadd, id).Scan(&accountinfo.smtp_add)
+	db.QueryRow("SELECT smtp_port FROM mail_info WHERE mail=$1 and id=$2", mailadd, id).Scan(&accountinfo.smtp_port)
+	db.QueryRow("SELECT imap_add FROM mail_info WHERE mail=$1 and id=$2", mailadd, id).Scan(&accountinfo.imap_add)
+	db.QueryRow("SELECT imap_port FROM mail_info WHERE mail=$1 and id=$2", mailadd, id).Scan(&accountinfo.imap_port)
+	db.QueryRow("SELECT pop3_add FROM mail_info WHERE mail=$1 and id=$2", mailadd, id).Scan(&accountinfo.pop3_add)
+	db.QueryRow("SELECT pop3_port FROM mail_info WHERE mail=$1 and id=$2", mailadd, id).Scan(&accountinfo.pop3_port)
+	db.QueryRow("SELECT convert_from(decrypt(decode(mail_passwd, 'hex'), 'epjtwihnsasdc', 'aes'),'utf-8') from mail_info WHERE mail=$1 and id=$2", mailadd, id).Scan(&accountinfo.mail_passwd)
+	fmt.Println("DB_get아이디: ", accountinfo.smtp_add, accountinfo.smtp_port, accountinfo.imap_add, accountinfo.imap_port)
 	return accountinfo
 }
 
@@ -76,7 +89,16 @@ func getPresetInfo(db *sql.DB, mailadd string) accountInfo {
 	db.QueryRow("SELECT imap_port FROM mail_preset WHERE mail_add=$1", mailadd).Scan(&accountinfo.imap_port)
 	db.QueryRow("SELECT pop3_add FROM mail_preset WHERE mail_add=$1", mailadd).Scan(&accountinfo.pop3_add)
 	db.QueryRow("SELECT pop3_port FROM mail_preset WHERE mail_add=$1", mailadd).Scan(&accountinfo.pop3_port)
-	//db.QueryRow("SELECT convert_from(decrypt(decode(mail_passwd, 'hex'), 'epjtwihnsasdc', 'aes'),'utf-8') from mail_info WHERE mail=$1", mailadd).Scan(&accountinfo.mail_passwd)
 
 	return accountinfo
+}
+func goDotEnvVariable(key string) string {
+
+	err := godotenv.Load("config.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
