c5384ae85b4aa705ea7a1fa31110ab0381cb3d47
mcocma <mcocma@naver.com>
2020-11-27 09:30:07 +0900

    이메일 암호화 한글버전 메인페이지 내 메일계정 불러오는 로직 수정

--- mail_encryption/routes/mainpage.go
+++ mail_encryption/routes/mainpage.go
@@ -16,30 +16,30 @@ import (
 
 func index(w http.ResponseWriter, r *http.Request) {
 	LoggedIn(w, r, "/login")
-	t, err := template.ParseFiles("public/menu_index.html", "public/_head.html", "public/_menu.html", "public/_header.html", "public/_table.html")
+	t, err := template.ParseFiles("public/menu_index.html", "public/_head.html", "public/_menu.html", "public/_header.html", "public/_table.html", "public/_headjs.html")
 	if err != nil {
 		panic(err)
 	}
 
-	_, v1 := AllSessions(w, r)
+	v, v1 := AllSessions(w, r)
+	id := fmt.Sprintf("%v", v)
 	mailadd := fmt.Sprintf("%v", v1)
-	fmt.Println(":::::::", mailadd)
 
 	if mailadd != "<nil>" && mailadd != "" && mailadd != "[]" {
-		t.Execute(w, imapPage(mailadd, "1"))
+		t.Execute(w, imapPage(id, mailadd, "1"))
 	} else {
 		t.Execute(w, nil)
 	}
 }
 
-func imapPage(s string, num string) TodoPageData {
+func imapPage(id string, s string, num string) TodoPageData {
 	log.Println("Connecting to server...")
 	defer func() {
 		if r := recover(); r != nil {
-
 			fmt.Println("OPEN ERROR", r)
 		}
 	}()
+
 	imap.CharsetReader = charset.Reader
 	mailadd := s
 
@@ -47,7 +47,7 @@ func imapPage(s string, num string) TodoPageData {
 	db := connectDB()
 	defer disconnectDB(db)
 
-	accountinfo := getAccountInfo(db, mailadd)
+	accountinfo := getAccountInfo(db, mailadd, id)
 
 	hostaddr := (accountinfo.imap_add + ":" + accountinfo.imap_port)
 	log.Println("host: ", hostaddr)
@@ -67,8 +67,6 @@ func imapPage(s string, num string) TodoPageData {
 	if err := c.Login(mailadd, accountinfo.mail_passwd); err != nil {
 		fmt.Println("로그인 에러")
 		log.Panic(err)
-		//로그인 에러
-		//2020/06/10 10:21:48 Sorry, IMAP UserAuth Server Is Checking..Please, Try later.
 	}
 	log.Println("Logged in")
 
@@ -97,14 +95,11 @@ func imapPage(s string, num string) TodoPageData {
 	}
 	log.Println("Flags for INBOX:", mbox.Flags)
 
-	// Get the last 4 messages
+	// Get the last 50 messages
 	from := uint32(1)
-	tonum := mbox.Messages
-	if mbox.Messages > 15 {
-		// We're using unsigned integers here, only substract if the result is > 0
-		from = mbox.Messages - 14
-	}
-	fmt.Println("박스 개수: ", mbox.Messages)
+	tonum := mbox.Messages // 메일 총개수
+	if mbox.Messages > 50 {
+		from = mbox.Messages - 49
 
 	seqset := new(imap.SeqSet)
 	seqset.AddRange(from, tonum)
@@ -113,14 +108,13 @@ func imapPage(s string, num string) TodoPageData {
 	done = make(chan error, 1)
 	var section imap.BodySectionName
 	items := []imap.FetchItem{section.FetchItem()}
+
 	go func() {
 		done <- c.Fetch(seqset, items, messages)
 	}()
 
-	log.Println("Last 4 messages:")
-
 	data := TodoPageData{
-		Todos: make([]Todo, 21),
+		Todos: make([]Todo, 55),
 	}
 	if tonum == 0 {
 		return data
@@ -128,13 +122,11 @@ func imapPage(s string, num string) TodoPageData {
 
 	data.Num = num
 	if num == "2" {
-		data.Who = "받는 사람"
+		data.Who = "To"
 	} else {
-		data.Who = "보낸 사람"
+		data.Who = "From"
 	}
-
 	var i = 0
-
 	for msg := range messages {
 		r := msg.GetBody(&section)
 		mr, err := mail.CreateReader(r)
@@ -144,19 +136,21 @@ func imapPage(s string, num string) TodoPageData {
 		header := mr.Header
 
 		if date, err := header.Date(); err == nil {
-			//fmt.Println("date: ", date.UTC())
 			t := date.UTC()
-			t = t.In(time.FixedZone("KST", 9*60*60)) // 한국시간으로 바꾸기
+			t = t.In(time.FixedZone("SST", 8*60*60)) // Singapol Region
 			data.Todos[i].Date = (t.Format("2006-01-02 15:04:05"))
 
 		}
 		if num != "2" { //"_table" 보낸메일함은 받는 사람으로
 			if from, err := header.AddressList("From"); err == nil {
-				data.Todos[i].Sender = from[0].Address
+				if from != nil {
+					data.Todos[i].Sender = from[0].Address
+				} else {
+					data.Todos[i].Sender = "<null>"
+				}
 			}
 		} else {
-			if to, err := header.AddressList("To"); err == nil && len(to) != 0 { // to가 존재하지 않으면 오류 발생 - 제외
-				//runtime error: index out of range [0] with length 0
+			if to, err := header.AddressList("To"); err == nil && len(to) != 0 { 
 				data.Todos[i].Sender = to[0].Address
 			} else {
 				data.Todos[i].Sender = ""
@@ -170,24 +164,20 @@ func imapPage(s string, num string) TodoPageData {
 				data.Todos[i].Title = subject
 			}
 		}
-
 		data.Todos[i].Done = true
 		data.Todos[i].Uid = msg.SeqNum
+		//fmt.Println("UID: ", msg.SeqNum)
 		i++
-
 	}
 
 	if err := <-done; err != nil {
 		log.Panic(err)
-
 	}
-	//c.SetDebug(os.Stdout)
 	log.Println("Done!")
-
+	//메일 순서 변경
 	for i, j := 0, len(data.Todos)-1; i < j; i, j = i+1, j-1 {
 		data.Todos[i], data.Todos[j] = data.Todos[j], data.Todos[i]
 	}
-
 	return data
 }
 
@@ -225,22 +215,11 @@ func mailList(w http.ResponseWriter, r *http.Request) {
 		b := []byte(indivserver + ";")
 		mailservers = append(mailservers, b...)
 	}
-	//	_, v1 := AllSessions(w, r)
-	//c, ok := v1.(*[]byte)
-	//	eid := fmt.Sprintf("%v", v1)
-	//mailservers = append(mailservers, **c)
-	//fmt.Print(mailservers)
 
 	if r.Body == nil {
 		http.Error(w, "Please send a request body", 400)
 		return
 	}
-
-	//	b, _ := ioutil.ReadAll(r.Body)
-
-	//s := string(b)
-
-	//fmt.Println("받은 검색 내용: ", s)
 
 	w.Write(mailservers)
 }
@@ -251,8 +230,6 @@ func mailChange(w http.ResponseWriter, r *http.Request) {
 	url, _ := r.URL.Query()["url"]
 	url1 := strings.Join(url, "")
 	url1 = "/" + url1
-	//fmt.Println("url1: ", url1)
-	//fmt.Println(email[0], ok)
 	session := GetSession(w, r)
 
 	session.Values["eId"] = email[0]
@@ -262,7 +239,4 @@ func mailChange(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("in")
 		w.Write([]byte("1"))
 	}
-
-	//fmt.Println("1: ", url)
-	//w.Write([]byte("1"))
 }
